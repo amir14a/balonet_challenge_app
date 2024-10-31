e22aebe9b2cc4a1f16cd21abfc4981266635863c
AmirAbbas-PC <a.abbasj@yahoo.com>
2024-10-31 19:25:51 +0330

    Add night mode button

--- app/src/main/java/com/aaj/balonetchallengeapplication/view/activity/MainActivity.kt
+++ app/src/main/java/com/aaj/balonetchallengeapplication/view/activity/MainActivity.kt
@@ -2,11 +2,14 @@ package com.aaj.balonetchallengeapplication.view.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import androidx.core.view.GravityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.aaj.balonetchallengeapplication.R
 import com.aaj.balonetchallengeapplication.databinding.ActivityMainBinding
+import com.aaj.balonetchallengeapplication.util.StaticParameters
 import com.aaj.balonetchallengeapplication.viewmodel.MainActivityViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,6 +27,25 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainActivityViewModel>(
         }
         binding.mainNavHost.post {
             binding.bottomNavigationView.setupWithNavController(binding.mainNavHost.findNavController())
+        }
+
+        binding.navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.clearCache -> {
+                }
+            }
+            return@setNavigationItemSelectedListener true
+        }
+        val nightModeSwitch = binding.navView.menu.findItem(R.id.nightMode).actionView
+            ?.findViewById<SwitchCompat>(R.id.nightModeSwitch)
+        nightModeSwitch?.isChecked = StaticParameters.isNightMode
+        nightModeSwitch?.setOnCheckedChangeListener { _, value ->
+            StaticParameters.isNightMode = value
+            viewModel.saveNightMode(value)
+            if (StaticParameters.isNightMode)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            else
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
     }
 
